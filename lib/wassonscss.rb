b31a8b6025a8a8d929433d220c544812e829a753
austinmcdowell <austin.p.mcdowell@gmail.com>
2021-07-19 14:49:30 -0600

    Added wasson.scss

--- lib/wassonscss.rb
+++ lib/wassonscss.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "wassonscss/version"
+require "wassonscss/wasson.scss"
 
 module Wassonscss
   class Error < StandardError; end
-  # Your code goes here...
+  class Dryer
+    def dry 
+      `say Sup`
+    end
+  end
 end
